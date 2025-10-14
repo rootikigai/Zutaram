c5e49d21ba2227d11b32a81c674926e683b80433
rootikigai <ifykel360@gmail.com>
2025-10-14 21:16:38 +0100

    Add product API with Redux Toolkit integration

---
+++ src/apis/productApi.jsx
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const URL = import.meta.env.VITE_BASE_URL;
+
+export const productApi = createApi({
+  reducerPath: "products",
+  baseQuery: fetchBaseQuery({
+    baseUrl: URL
+  }),
+    endpoints: (builder) => ({
+        getAllProducts: builder.query({
+            query: () => ("/products")
+        })
+    })
+})
+
+export const { useGetAllProductsQuery } = productApi;
