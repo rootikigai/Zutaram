6ad0dff68519570c30adb83e1f7a32c4ad03bbc8
rootikigai <ifykel360@gmail.com>
2025-10-14 21:16:07 +0100

    Add initial App component with router integration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import router from "./routes/router";
+import { RouterProvider } from "react-router";
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App;
